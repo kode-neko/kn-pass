7462dc51fd361a70b6bd7f89c8cb62c5885448e4
Kodeneko <ladysun.freedom@gmail.com>
2022-10-15 15:33:03 +0200

    [dev] dummy check file

---
+++ check.js
@@ -0,0 +1,23 @@
+function encode() {
+  return 'test';
+}
+function encodeCustom(pass) {
+  return 'test';
+}
+function decode(pass, salt, hash) {
+  return 'test';
+}
+function token(pass, user, secret) {
+  return 'test';
+}
+function checkToken(token, secret) {
+  return 'test';
+}
+
+export {
+  encode,
+  encodeCustom,
+  decode,
+  token,
+  checkToken
+}
