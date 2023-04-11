717aab7496298862985b6d605d381f21e9522f71
Kodeneko <ladysun.freedom@gmail.com>
2023-04-11 02:22:03 +0200

    [dev] change scripts & title message

--- check.js
+++ check.js
@@ -1,37 +1,40 @@
-import bcrypt from 'bcrypt';
-import randomatic from 'randomatic';
-import { default as jwt } from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import randomatic from "randomatic";
+import {default as jwt} from "jsonwebtoken";
+
+function key(length) {
+  const key = randomatic("Aa0!", length);
+  return {key};
+}
 
 function encode(length) {
-  const pass = randomatic('Aa0!', length);
+  const pass = randomatic("Aa0!", length);
   const salt = bcrypt.genSaltSync(length);
   const hash = bcrypt.hashSync(pass, salt);
-  return { pass, salt, hash }
+  return {pass, salt, hash};
 }
+
 function encodeCustom(pass, length) {
   const salt = bcrypt.genSaltSync(length);
   const hash = bcrypt.hashSync(pass, salt);
-  return { pass, salt, hash }
+  return {pass, salt, hash};
 }
+
 function decode(pass, salt, hash) {
   const hashCheck = bcrypt.hashSync(pass, salt);
   const isPass = hash === hashCheck;
-  return { isPass }
-}
-function createToken(user, secret) {
-  console.log('create')
-  const token = jwt.sign({ user }, secret, { expiresIn: 1 * 60 * 60 * 1000 });
-  return { token }
-}
-function checkToken(token, secret) {
-  const isVerify = jwt.verify(token, secret);
-  return { isVerify }
+  return {isPass};
 }
 
-export {
-  encode,
-  encodeCustom,
-  decode,
-  createToken,
-  checkToken
+function createToken(user, secret) {
+  console.log("create");
+  const token = jwt.sign({user}, secret, {expiresIn: 1 * 60 * 60 * 1000});
+  return {token};
 }
+
+function checkToken(token, secret) {
+  const isVerify = jwt.verify(token, secret);
+  return {isVerify};
+}
+
+export {key, encode, encodeCustom, decode, createToken, checkToken};
--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-import figlet from 'figlet';
-import chalk from 'chalk';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import {
-  Options,
-  OptionsConfig
-} from './options.js';
+import figlet from "figlet";
+import chalk from "chalk";
+import yargs from "yargs";
+import {hideBin} from "yargs/helpers";
+import {Options, OptionsConfig} from "./options.js";
 import {
   encode as encodeF,
   encodeCustom as encodeCustomF,
   decode as decodeF,
   createToken as createTokenF,
-  checkToken as checkTokenF
-} from './check.js';
+  checkToken as checkTokenF,
+} from "./check.js";
 
 function title() {
-  console.log(chalk.bold.green(
-    figlet.textSync('KnPass', {
-      font: 'DOS Rebel',
-      horizontalLayout: 'default',
-      verticalLayout: 'default'
-    })
-  ))
+  console.log(
+    chalk.bold.green(
+      figlet.textSync("KnPass", {
+        font: "DOS Rebel",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+      })
+    )
+  );
 }
 
-function print(infoList) {
-  console.log(chalk.bgGreen('Result'))
-  Object.keys(infoList).forEach(key =>
-    console.log(key, infoList[key])
-  );
+function print(op, infoList) {
+  console.log(chalk.bgGreen(`Result ${op} `));
+  Object.keys(infoList).forEach((key) => console.log(key, "\t", infoList[key]));
 }
 
 function getArguments() {
   const argsv = yargs(hideBin(process.argv))
-    .locale('en')
+    .locale("en")
     .option(Options.ENCODE, OptionsConfig[Options.ENCODE])
     .option(Options.ENCODE_CUSTOM, OptionsConfig[Options.ENCODE_CUSTOM])
     // .option(Options.DECODE, OptionsConfig[Options.DECODE])
@@ -45,40 +42,56 @@ function getArguments() {
     .option(Options.SALT, OptionsConfig[Options.SALT])
     .option(Options.SECRET_TOKEN, OptionsConfig[Options.SECRET_TOKEN])
     .option(Options.TOKEN, OptionsConfig[Options.TOKEN])
-    .option(Options.LENGTH, OptionsConfig[Options.LENGTH])
-    .argv
+    .option(Options.LENGTH, OptionsConfig[Options.LENGTH]).argv;
   return argsv;
 }
 
 function operation(args) {
   const {
-    encode, encodeCustom, decode,
-    createToken, checkToken,
-    user, pass, hash, salt,
-    secretToken, token, length
-  } = args
+    encode,
+    encodeCustom,
+    decode,
+    createToken,
+    checkToken,
+    user,
+    pass,
+    hash,
+    salt,
+    secretToken,
+    token,
+    length,
+  } = args;
+  let op;
   let msj;
   if (encode) {
+    op = "Encode";
     msj = encodeF(length);
   } else if (encodeCustom && pass) {
-    msj = encodeCustomF(pass, length)
+    op = "Encode Custom";
+    msj = encodeCustomF(pass, length);
   } else if (decode && pass && salt && hash) {
-    msj = decodeF(pass, salt, hash)
+    op = "Decode Pass Salt";
+    msj = decodeF(pass, salt, hash);
   } else if (createToken && user && secretToken) {
-    msj = createTokenF(user, secretToken)
+    op = "Create Token";
+    msj = createTokenF(user, secretToken);
   } else if (checkToken && token && secretToken) {
-    msj = checkTokenF(token, secretToken)
+    op = "Check Token";
+    msj = checkTokenF(token, secretToken);
   } else {
-    msj = { err: 'There was a problem. Pelease use "help" option to follow teh indications.' }
+    op = "";
+    msj = {
+      err: 'There was a problem. Pelease use "help" option to follow teh indications.',
+    };
   }
-  print(msj)
+  print(op, msj);
 }
 
 function init() {
   title();
   const args = getArguments();
   // console.log(args)
-  operation(args)
+  operation(args);
 }
 
 init();
