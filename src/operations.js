c7b9e444b06e2d0aaea1fe91abdbe0550f7c571f
Kodeneko <ladysun.freedom@gmail.com>
2023-04-18 18:38:26 +0200

    [dev] Mega refactor

--- src/operations.js
+++ src/operations.js
@@ -3,51 +3,51 @@ import randomatic from "randomatic";
 import {default as jwt} from "jsonwebtoken";
 import {generateKeySync} from "node:crypto";
 
-function createKey(length) {
+function opCreateKey(length) {
   const key = generateKeySync("aes", {length}).export().toString("hex");
   return {key};
 }
 
-function createPass(length) {
+function opCreatePass(length) {
   const pass = randomatic("Aa0!", length);
   return {pass};
 }
 
-function encode(length) {
+function opCreateHash(length) {
   const pass = randomatic("Aa0!", length);
   const salt = bcrypt.genSaltSync(length);
   const hash = bcrypt.hashSync(pass, salt);
   return {pass, salt, hash};
 }
 
-function encodeCustom(pass) {
+function opCreateHashCustom(pass) {
   const salt = bcrypt.genSaltSync();
   const hash = bcrypt.hashSync(pass, salt);
   return {pass, salt, hash};
 }
 
-function decode(pass, hash) {
+function opCheckHash(pass, hash) {
   console.log(hash);
   const isPass = bcrypt.compareSync(pass, hash);
   return {isPass};
 }
 
-function createToken(user, secret) {
-  const token = jwt.sign({user}, secret, {expiresIn: 1 * 60 * 60 * 1000});
+function opCreateToken(user, secretKey) {
+  const token = jwt.sign({user}, secretKey, {expiresIn: 1 * 60 * 60 * 1000});
   return {token};
 }
 
-function checkToken(token, secret) {
-  const isVerify = jwt.verify(token, secret);
+function opCheckToken(token, secretKey) {
+  const isVerify = jwt.verify(token, secretKey);
   return {isVerify};
 }
 
 export {
-  createKey,
-  createPass,
-  encode,
-  encodeCustom,
-  decode,
-  createToken,
-  checkToken,
+  opCreateKey,
+  opCreatePass,
+  opCreateHash,
+  opCreateHashCustom,
+  opCheckHash,
+  opCreateToken,
+  opCheckToken,
 };
